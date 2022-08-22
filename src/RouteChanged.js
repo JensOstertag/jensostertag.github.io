e1053a2d7c06b5a63f0b5a458d0eff95f19e995b
JensOstertag <JensOstertag@gmx.de>
2022-08-22 15:39:03 +0200

    Initialize Viewport on Route Change

--- src/RouteChanged.js
+++ src/RouteChanged.js
@@ -1,10 +1,13 @@
 import { useLayoutEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+import * as Viewport from './viewport.js';
+
 function ScrollToTop({ children }) {
     const location = useLocation();
     useLayoutEffect(() => {
         window.scrollTo(0, 0);
+        Viewport.init();
     }, [location.pathname]);
 
     return ( children );
