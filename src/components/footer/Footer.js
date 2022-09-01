2913a83731a805afd36d677a4dec89fa8cd51b65
JensOstertag <JensOstertag@gmx.de>
2022-09-01 09:48:19 +0200

    Optimize Footer for Mobile Users

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component {
                             <img src={process.env.PUBLIC_URL + "/img/icon.svg"} alt="" />
                             Jens Ostertag
                         </div>
-                        <p className="copyright">&copy; 2020 - {new Date().getFullYear()} &nbsp; Jens Ostertag</p>
+                        <span><p className="copyright">&copy; 2020 - {new Date().getFullYear()} &nbsp; </p></span><span><p>Jens Ostertag</p></span>
                         <p className="version"><img src={process.env.PUBLIC_URL + "/img/ico/gear.svg"} className="gear" alt="gear" /> Version: {websiteConfig.version}</p>
                     </div>
                 </div>
